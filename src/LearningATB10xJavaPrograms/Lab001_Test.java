e449d3361cd34e60162bf23235f5687d77d44b8f
Lasya2580 <ambatilasya53@gmail.com>
2025-01-06 21:27:16 +0530

    GitHub

---
+++ src/LearningATB10xJavaPrograms/Lab001_Test.java
@@ -0,0 +1,8 @@
+package LearningATB10xJavaPrograms;
+
+public class Lab001_Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println("JAVA");
+    }
+}
