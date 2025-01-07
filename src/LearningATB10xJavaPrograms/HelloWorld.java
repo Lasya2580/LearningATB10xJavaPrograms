965c0452e27e0daec9867a043188e30a178f3e4f
Lasya2580 <ambatilasya53@gmail.com>
2025-01-07 16:40:16 +0530

    Hello

--- src/LearningATB10xJavaPrograms/HelloWorld.java
+++ src/LearningATB10xJavaPrograms/HelloWorld.java
@@ -1,4 +1,9 @@
 package LearningATB10xJavaPrograms;
 
+import java.sql.SQLOutput;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
 }
