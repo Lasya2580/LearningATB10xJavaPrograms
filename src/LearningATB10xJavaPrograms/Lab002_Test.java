93d8e91265bb0a9aa1fb5090f12eeec102199a5a
Lasya2580 <ambatilasya53@gmail.com>
2025-02-02 09:54:19 +0530

    Hello

---
+++ src/LearningATB10xJavaPrograms/Lab002_Test.java
@@ -0,0 +1,12 @@
+package LearningATB10xJavaPrograms;
+
+public class Lab002_Test {
+    public static void main(String[] args) {
+        System.out.println("Lasya");
+    }
+
+    public static void main() {
+        System.out.println("Java");
+
+    }
+}
---
+++ src/LearningATB10xJavaPrograms/ex_01_JavaBasics/Lab001_HelloWorld.java
@@ -0,0 +1,9 @@
+package LearningATB10xJavaPrograms.ex_01_JavaBasics;
+
+import java.sql.SQLOutput;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+    }
+}
