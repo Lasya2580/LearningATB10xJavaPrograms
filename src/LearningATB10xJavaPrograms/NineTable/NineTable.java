9264db5d4c6a99b4cfa1dee11db7c141e40547bf
Lasya2580 <ambatilasya53@gmail.com>
2025-02-10 20:46:50 +0530

    Hello

---
+++ src/LearningATB10xJavaPrograms/NineTable/NineTable.java
@@ -0,0 +1,10 @@
+package LearningATB10xJavaPrograms.NineTable;
+
+public class NineTable {
+    public static void main(String[] args) {
+        int n=9;
+       for (int i=1;i<=10;i++)
+        System.out.println(n + "*" + i + "=" + n*i);
+
+    }
+}
---
+++ src/LearningATB10xJavaPrograms/ex_01_JavaBasics/Lab002_TestMain.java
@@ -0,0 +1,12 @@
+package LearningATB10xJavaPrograms.ex_01_JavaBasics;
+
+public class Lab002_TestMain {
+    public static void main(String[] args) {
+        System.out.println("Lasya");
+    }
+
+    public static void main() {
+        System.out.println("Test");
+
+    }
+}
